825f3eae3d8da0844239eed372089cacd444523f
k3y5tr0k3 <bryandonnelly7@gmail.com>
2025-01-30 06:36:04 +0000

    Add CPU and Test websocket routing patterns to asgi config

--- sysmonify/sysmonify/asgi.py
+++ sysmonify/sysmonify/asgi.py
@@ -10,13 +10,21 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from dashboard.routing import websocket_urlpatterns
+from dashboard.routing import websocket_urlpatterns as dashboard_websocket_urlpatterns
+from sysmonify.core.routing import websocket_urlpatterns as test_websocket_urlpatterns
+from cpu.routing import websocket_urlpatterns as cpu_websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sysmonify.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                dashboard_websocket_urlpatterns
+                + test_websocket_urlpatterns
+                + cpu_websocket_urlpatterns
+            )
+        ),
     }
 )
