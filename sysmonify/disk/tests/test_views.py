bb636c26598f5976b787e0742b71c86cf03ae958
k3y5tr0k3 <bryandonnelly7@gmail.com>
2025-02-06 01:28:52 +0000

    Improve docstring for disk view tests

--- sysmonify/disk/tests/test_views.py
+++ sysmonify/disk/tests/test_views.py
@@ -11,11 +11,19 @@ class TestDiskView(TestCase):
     """Test class for CpuView."""
 
     def test_disk_view_status_code(self):
-        """Test that the disk view renders with a status code 200."""
+        """Test that the disk view renders with a status code 200.
+
+        Asserts:
+            Response status code is '200'
+        """
         response = self.client.get(reverse("disks"))
         self.assertEqual(response.status_code, 200)
 
     def test_disk_view_template_used(self):
-        """Test that the correct template is used for the disk view."""
+        """Test that the correct template is used for the disk view.
+
+        Asserts:
+            The correct template is rendered.
+        """
         response = self.client.get(reverse("disks"))
         self.assertTemplateUsed(response, "disks.html")
