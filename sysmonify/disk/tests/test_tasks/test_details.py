f59d0b65ab2b7f0c68af0e51b20997c8c4e9e871
k3y5tr0k3 <bryandonnelly7@gmail.com>
2025-02-06 01:34:17 +0000

    Improve docstring for disk details tests

--- sysmonify/disk/tests/test_tasks/test_details.py
+++ sysmonify/disk/tests/test_tasks/test_details.py
@@ -21,7 +21,13 @@ class DiskDetailsTestCase(TestCase):
 
     @unittest.mock.patch("subprocess.run")
     def test_get_raw_block_devices_success(self, mock_subprocess):
-        """Test _get_raw_block_devices when lsblk returns valid JSON data."""
+        """Test _get_raw_block_devices when lsblk returns valid JSON data.
+
+        Asserts:
+            - A list is returned.
+            - The correct number of element are present in the list.
+            - The name of the first block device is what is expected.
+        """
         mock_output = json.dumps(
             {
                 "blockdevices": [
@@ -39,7 +45,12 @@ class DiskDetailsTestCase(TestCase):
 
     @unittest.mock.patch("pathlib.Path.resolve")
     def test_is_physical_disk(self, mock_resolve):
-        """Test _is_physical_disk to differentiate physical and virtual disks."""
+        """Test _is_physical_disk to differentiate physical and virtual disks.
+
+        Asserts:
+            - `True` is returned for physical disks.
+            - `False` is returned for virtual disks.
+        """
         mock_resolve.return_value = "/sys/devices/pci0000:00/0000:00:1f.2/ata1/host0/target0:0:0/0:0:0:0/block/sda"
         self.assertTrue(self.disk_details._is_physical_disk("sda"))
 
@@ -47,7 +58,12 @@ class DiskDetailsTestCase(TestCase):
         self.assertFalse(self.disk_details._is_physical_disk("zd0"))
 
     def test_get_physical_disks_from_raw_block_devices(self):
-        """Test filtering physical disks from block devices."""
+        """Test filtering physical disks from block devices.
+
+        Asserts:
+            - disks that should be filtered out are removed (e.g. loopback devices).
+            - disks that should remain are present in return value (e.g. disks).
+        """
         raw_devices = [
             {"name": "sda", "type": "disk"},
             {"name": "sdb", "type": "disk"},
@@ -74,7 +90,11 @@ class DiskDetailsTestCase(TestCase):
         DiskDetails, "_get_physical_disks_from_raw_block_devices"
     )
     def test_get_details(self, mock_get_physical, mock_get_raw):
-        """Test get_details method to ensure it returns filtered physical disks."""
+        """Test get_details method to ensure it returns filtered physical disks.
+
+        Asserts:
+            Only details of physical disks are returned.
+        """
         mock_get_raw.return_value = [
             {"name": "sda", "type": "disk"},
             {"name": "sda1", "type": "part"},
