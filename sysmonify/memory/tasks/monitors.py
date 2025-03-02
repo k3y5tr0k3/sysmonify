ea79119dae2604b59dcda3245f01309c6d760836
k3y5tr0k3 <bryandonnelly7@gmail.com>
2025-03-02 08:38:32 +0000

    add logging to memory monitor task

--- sysmonify/memory/tasks/monitors.py
+++ sysmonify/memory/tasks/monitors.py
@@ -7,7 +7,12 @@ Classes:
         A class for retrieving real-time memory and swap stats.
 """
 
+import logging
+
 from sysmonify.core.tasks import Monitor
+
+
+logger = logging.getLogger(__name__)
 
 
 class MemoryMonitor(Monitor):
@@ -32,27 +37,48 @@ class MemoryMonitor(Monitor):
             "swap": {"total": "0 KB", "free": "0 KB"},
         }
 
-        with open("/proc/meminfo", "r") as file:
-            for line in file:
-                if line.startswith("MemTotal"):
-                    total_mem = line.split(":")[1].strip()
-                    metrics["memory"]["total"] = total_mem
+        try:
+            with open("/proc/meminfo", "r") as file:
+                for line in file:
+                    try:
+                        if line.startswith("MemTotal"):
+                            total_mem = line.split(":")[1].strip()
+                            metrics["memory"]["total"] = total_mem
 
-                if line.startswith("MemFree"):
-                    free_mem = line.split(":")[1].strip()
-                    metrics["memory"]["free"] = free_mem
+                        if line.startswith("MemFree"):
+                            free_mem = line.split(":")[1].strip()
+                            metrics["memory"]["free"] = free_mem
 
-                if line.startswith("SwapTotal"):
-                    total_swap = line.split(":")[1].strip()
-                    metrics["swap"]["total"] = total_swap
+                        if line.startswith("SwapTotal"):
+                            total_swap = line.split(":")[1].strip()
+                            metrics["swap"]["total"] = total_swap
 
-                if line.startswith("SwapFree"):
-                    free_swap = line.split(":")[1].strip()
-                    metrics["swap"]["free"] = free_swap
+                        if line.startswith("SwapFree"):
+                            free_swap = line.split(":")[1].strip()
+                            metrics["swap"]["free"] = free_swap
+
+                    except ValueError as e:
+                        logger.error(
+                            f"ValueError while parsing line: {line}. Error: {e}"
+                        )
+
+                    except IndexError as e:
+                        logger.error(
+                            f"IndexError while parsing line: {line}. Error: {e}"
+                        )
+
+                    except Exception as e:
+                        logger.error(
+                            f"Unexpected error while parsing line: {line}. Error: {e}"
+                        )
+
+        except FileNotFoundError:
+            logger.error("/proc/meminfo file not found.")
+
+        except IOError as e:
+            logger.error(f"IOError while opening /proc/meminfo: {e}")
+
+        except Exception as e:
+            logger.error(f"Unexpected error while accessing /proc/meminfo: {e}")
 
         return metrics
-
-
-if __name__ == "__main__":
-    metrics = MemoryMonitor().get_metrics()
-    print(metrics)
